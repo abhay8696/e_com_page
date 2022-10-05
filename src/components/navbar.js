749be27172fb355c4cc83a28b4517d6fcadbbcf8
abhay8696 <abhaykamble1346@gmail.com>
2022-10-05 19:57:51 +0530

    navbar is ready and responsive with cart function

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,9 @@ import logo from '../data/images/logo.svg';
 import cart from '../data/images/icon-cart.svg';
 import profileImage from '../data/images/image-avatar.png';
 const Navbar = () => {
+    //states
     const [ sideBar, setSideBar ] = useState(false);
+    const [ openCart, setOpenCart ] = useState(false);
 
     //functions
     const showSideBar = ()=> {
@@ -21,6 +23,16 @@ const Navbar = () => {
                     <div>Contact</div>
                 </div>
                 <div className='blankSidebarBody' onClick={()=> setSideBar(false)}></div>
+            </div>
+        )
+    },
+    showCart = ()=> {
+        return(
+            <div className='cartDiv'>
+                <div className='cartHead'>Cart</div>
+                <div className='cartBody'>
+                    <span>Your cart is empty.</span>
+                </div>
             </div>
         )
     }
@@ -45,13 +57,14 @@ const Navbar = () => {
             </div>
             <div className='cart_profile'>
                 {/* <img src={cart} /> */}
-                <svg viewBox="0 0 24 24">
+                <svg viewBox="0 0 24 24" onClick={()=> setOpenCart(!openCart)}>
                 <path xmlns="http://www.w3.org/2000/svg" d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"/>
                 </svg>
                 <img src={profileImage} className='profile_image'/>
             </div>
         </div>
         {sideBar ? showSideBar() : null}
+        {openCart ? showCart() : null}
         </>
     );
 };
