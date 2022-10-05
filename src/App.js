f1373612a15d98f648a9a7ee90d2437b45311d13
abhay8696 <abhaykamble1346@gmail.com>
2022-10-05 23:29:39 +0530

    images previews added and made responsive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import ImageDiv from './components/imageDiv';
+import ProductInfo from './components/productInfo';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className='body'>
-        <div className='imageDiv'>
-
-        </div>
-        <div className='productFunc'>
-
-        </div>
+        <ImageDiv />
+        <ProductInfo />
       </div>
     </div>
   );
---
+++ src/components/productInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductInfo = () => {
+    return (
+        <div className='productInfo'>
+            <h1>Product Info</h1>
+        </div>
+    );
+};
+
+export default ProductInfo;
