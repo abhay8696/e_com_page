e317954bf543e975756251583bdd405b0525b8e6
abhay8696 <abhaykamble1346@gmail.com>
2022-10-13 21:47:42 +0530

    preview of selected shoe is updated in app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Navbar from './components/navbar';
 import ImageDiv from './components/imageDiv';
 import ProductInfo from './components/productInfo';
+import { useState } from 'react';
+
+//images
+import image1 from './data/images/image-product-1.jpg'
+import image2 from './data/images/image-product-2.jpg'
+import image3 from './data/images/image-product-3.jpg'
+import image4 from './data/images/image-product-4.jpg'
 
 function App() {
+  const shoeImages= [image1, image2, image3, image4];
+  const [ selected, setSelected ] = useState([shoeImages[0], 1]);
+
+  //functions
+  const 
+  chooseShoe = num=> {
+    if(num >= 5){
+      return setSelected([shoeImages[0], 1]);
+    }
+    else if(num <= 0){
+      return setSelected([shoeImages[3], 4]);
+    }
+    return setSelected([shoeImages[num-1], num])
+  }
   return (
     <div className="App">
       <Navbar />
       <div className='body'>
-        <ImageDiv />
+        <ImageDiv chooseShoe={chooseShoe} shoeImages={shoeImages} selected={selected}/>
         <ProductInfo />
       </div>
     </div>
--- src/components/imageDiv.js
+++ src/components/imageDiv.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/imageDiv.css'
 
 //images
@@ -7,36 +7,30 @@ import image2 from '../data/images/image-product-2.jpg'
 import image3 from '../data/images/image-product-3.jpg'
 import image4 from '../data/images/image-product-4.jpg'
 
-const ImageDiv = () => {
-    let pic1 = image1, pic2 = image2, pic3 = image3, pic4 = image4;
-    const [ selectedImg, setSelectedImg ] = useState(1);
-    const changeBackground = (num)=> {
-        if(num === 5) num = 1;
-        else if(num === 0) num = 4;
-        setSelectedImg(num);
-    }
+const ImageDiv = props => {
+    const { selected, chooseShoe, shoeData } = props;
     return (
         <div className='imageDiv'>
-            <div className={`previewDiv${selectedImg}`}>
-                <img src={eval(`pic${selectedImg}`)} className='productImage'/>
+            <div className={`previewDiv${selected[1]}`}>
+                <img src={selected[0]} className='productImage'/>
                 <div className='arrowButt'>
-                    <svg onClick={()=> changeBackground(selectedImg-1)}
+                    <svg onClick={()=> chooseShoe((selected[1]-1))}
                         xmlns="http://www.w3.org/2000/svg" width="12" height="18">
                         <path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3"/>
                     </svg>
                 </div>
                 <div className='arrowButt'>
-                <svg onClick={()=> changeBackground(selectedImg+1)}
+                <svg onClick={()=> chooseShoe((selected[1]+1))}
                     xmlns="http://www.w3.org/2000/svg" width="13" height="18">
                     <path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3"/>
                 </svg>
                 </div>
             </div>
             <div className='thumbnails'>
-                <img src={image1} className='thumbImage' onClick={()=> setSelectedImg(1)}/>
-                <img src={image2} className='thumbImage' onClick={()=> setSelectedImg(2)}/>
-                <img src={image3} className='thumbImage' onClick={()=> setSelectedImg(3)}/>
-                <img src={image4} className='thumbImage' onClick={()=> setSelectedImg(4)}/>
+                <img src={image1} className='thumbImage' onClick={()=> chooseShoe(1)}/>
+                <img src={image2} className='thumbImage' onClick={()=> chooseShoe(2)}/>
+                <img src={image3} className='thumbImage' onClick={()=> chooseShoe(3)}/>
+                <img src={image4} className='thumbImage' onClick={()=> chooseShoe(4)}/>
             </div>
         </div>
     );
